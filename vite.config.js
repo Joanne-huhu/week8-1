9c0d9ca87fc4ec4d1e0bdb1c19b47fa702d59a26
Joannehu <qoohcy4120gmail.com>
2024-09-07 22:31:06 +0800

    navbar建立

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/week8/',
+  base: '/week8-1/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
